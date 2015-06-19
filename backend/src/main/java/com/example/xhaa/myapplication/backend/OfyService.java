138c2655c04b8f5839252b582d0bbd123884dd0b
iwansyahp <iwansyahp@gmail.com>
2015-06-20 02:59:48 +0700

    Tambah PasienRecord

--- backend/src/main/java/com/example/xhaa/myapplication/backend/OfyService.java
+++ backend/src/main/java/com/example/xhaa/myapplication/backend/OfyService.java
@@ -13,6 +13,7 @@ public class OfyService {
     static {
         ObjectifyService.register(RegistrationRecord.class);
         ObjectifyService.register(UserRecord.class);
+        //ObjectifyService.register(PasienRecord.class);
     }
 
     public static Objectify ofy() {
